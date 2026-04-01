3dbe1de7007e786299068d8a7c785c07b6d78e97
unknown <lu.lukosevicius@gmail.com>
2026-04-01 22:22:20 +0300

    Your commit message

--- src/main/java/system/controller/AIController.java
+++ src/main/java/system/controller/AIController.java
@@ -14,7 +14,7 @@ import java.util.Map;
 @CrossOrigin
 public class AIController {
 
-    //private final String API_KEY = "API KEY"; // <-- paste your key here
+    private final String API_KEY = "API KEY"; // <-- paste your key here
 
 
 @PostMapping
